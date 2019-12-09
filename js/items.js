397d67ca64d70bdc348df487aaa47375c1dc46a5
unknown <anjularatnayaka@gmail.com>
2019-12-09 21:09:31 +0530

    adding new category pages

--- js/items.js
+++ js/items.js
@@ -9,85 +9,85 @@
 // }
 
 var bestSellerItems = [
-    {
-        "itemName": "PS4 Pro",
-        "itemPrice": "$19.99",
-        "imgPath": "../Resources/image/Items/PS4.jpg"
-    },
-    {
-        "itemName": "PS4 Pro",
-        "itemPrice": "$19.99",
-        "imgPath": "../Resources/image/Items/PS4.jpg"
-    },
-    {
-        "itemName": "PS4 Pro",
-        "itemPrice": "$19.99",
-        "imgPath": "../Resources/image/Items/PS4.jpg"
-    },
-    {
-        "itemName": "PS4 Pro",
-        "itemPrice": "$19.99",
-        "imgPath": "../Resources/image/Items/PS4.jpg"
-    },
-    {
-        "itemName": "PS4 Pro",
-        "itemPrice": "$19.99",
-        "imgPath": "../Resources/image/Items/PS4.jpg"
-    },
+  {
+    itemName: "PS4",
+    itemPrice: "$19.99",
+    imgPath: "../Resources/image/Items/PS4.jpg"
+  },
+  {
+    itemName: "PS4",
+    itemPrice: "$19.99",
+    imgPath: "../Resources/image/Items/PS4.jpg"
+  },
+  {
+    itemName: "PS4",
+    itemPrice: "$19.99",
+    imgPath: "../Resources/image/Items/PS4.jpg"
+  },
+  {
+    itemName: "PS4",
+    itemPrice: "$19.99",
+    imgPath: "../Resources/image/Items/PS4.jpg"
+  },
+  {
+    itemName: "PS4",
+    itemPrice: "$19.99",
+    imgPath: "../Resources/image/Items/PS4.jpg"
+  }
 ];
 
 var hotDealItems = [
-    {
-        "itemName": "PS4 Pro",
-        "itemPrice": "$19.99",
-        "imgPath": "../Resources/image/Items/PS4.jpg"
-    },
-    {
-        "itemName": "PS4 Pro",
-        "itemPrice": "$19.99",
-        "imgPath": "../Resources/image/Items/PS4.jpg"
-    },
-    {
-        "itemName": "PS4 Pro",
-        "itemPrice": "$19.99",
-        "imgPath": "../Resources/image/Items/PS4.jpg"
-    },
-    {
-        "itemName": "PS4 Pro",
-        "itemPrice": "$19.99",
-        "imgPath": "../Resources/image/Items/PS4.jpg"
-    },
-    {
-        "itemName": "PS4 Pro",
-        "itemPrice": "$19.99",
-        "imgPath": "../Resources/image/Items/PS4.jpg"
-    },
+  {
+    itemName: "PS4",
+    itemPrice: "$19.99",
+    imgPath: "../Resources/image/Items/PS4.jpg"
+  },
+  {
+    itemName: "PS4",
+    itemPrice: "$19.99",
+    imgPath: "../Resources/image/Items/PS4.jpg"
+  },
+  {
+    itemName: "PS4",
+    itemPrice: "$19.99",
+    imgPath: "../Resources/image/Items/PS4.jpg"
+  },
+  {
+    itemName: "PS4",
+    itemPrice: "$19.99",
+    imgPath: "../Resources/image/Items/PS4.jpg"
+  },
+  {
+    itemName: "PS4",
+    itemPrice: "$19.99",
+    imgPath: "../Resources/image/Items/PS4.jpg"
+  }
 ];
 
 var newItems = [
-    {
-        "itemName": "PS4 Pro",
-        "itemPrice": "$19.99",
-        "imgPath": "../Resources/image/Items/PS4.jpg"
-    },
-    {
-        "itemName": "PS4 Pro",
-        "itemPrice": "$19.99",
-        "imgPath": "../Resources/image/Items/PS4.jpg"
-    },
-    {
-        "itemName": "PS4 Pro",
-        "itemPrice": "$19.99",
-        "imgPath": "../Resources/image/Items/PS4.jpg"
-    },
-    {
-        "itemName": "PS4 Pro",
-        "itemPrice": "$19.99",
-        "imgPath": "../Resources/image/Items/PS4.jpg"
-    },
-    {
-        "itemName": "PS4 Pro",
-        "itemPrice": "$19.99",
-        "imgPath": "../Resources/image/Items/PS4.jpg"
-    }, 
+  {
+    itemName: "PS4",
+    itemPrice: "$19.99",
+    imgPath: "../Resources/image/Items/PS4.jpg"
+  },
+  {
+    itemName: "PS4",
+    itemPrice: "$19.99",
+    imgPath: "../Resources/image/Items/PS4.jpg"
+  },
+  {
+    itemName: "PS4",
+    itemPrice: "$19.99",
+    imgPath: "../Resources/image/Items/PS4.jpg"
+  },
+  {
+    itemName: "PS4",
+    itemPrice: "$19.99",
+    imgPath: "../Resources/image/Items/PS4.jpg"
+  },
+  {
+    itemName: "PS4",
+    itemPrice: "$19.99",
+    imgPath: "../Resources/image/Items/PS4.jpg"
+  }
 ];
